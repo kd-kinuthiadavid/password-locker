661b1916b69d483268849570e187366737326c35
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-06-10 21:20:00 +0300

    edit to clear errors

--- password_locker.py
+++ password_locker.py
@@ -1,3 +1,54 @@
+
+def all():
+    print("Already have an account?(y/n)")
+    answer = input().lower()
+    if answer == 'n':
+        print("Enter your Name:")
+        username = input()
+        print("\n")
+        print("Enter A password")
+        password = input()
+        print("\n")
+        print("Enter your email")
+        email = input()
+        print("\n")
+
+
+        handle = open("credentials.txt","a")
+
+        handle.write(username)
+        handle.write(" ")
+        handle.write(password)
+        handle.write(" ")
+        handle.write(email)
+        handle.write("\n")
+
+
+        handle.close()
+        print("Account created")
+    elif answer == 'y':
+        login()
+
+def login():
+    print("Enter your username")
+    username = input()
+    print("\n")
+    print("Enter your password")
+    password = input()
+    print("\n")
+    print("Enter your email")
+    email = input()
+    for line in open("credentials.txt", "r").readlines():
+            myVar = line.split()
+            if username == myVar[0] and password == myVar[1]:
+                print("login succesfull")
+
+                return True
+            else:
+                print("Forgot username or password ?")
+
+                return False
+
 class User:
 
     """
@@ -16,48 +67,28 @@ class User:
         self.email = email
 
 
-    # def login():
-    #     print("Enter your username")
-    #     username = input()
+
+
+
+
+
+
+
+    # def add_user(self):
+    #     print("Hello There, welcome to Password Locker.")
     #     print("\n")
-    #     print("Enter your password")
-    #     password = input()
-    #     print("\n")
-    #     print("Enter your email")
-    #     email = input()
-
-    def add_user():
-        print("Hello There, welcome to Password Locker.")
-        print("\n")
-        print("Use the following shortcodes: ca - to create a account, lg - to login")
-        shortcodes = input().lower()
-        if shortcodes == 'ca':
-            print("Enter your Name:")
-            username = input()
-            print("\n")
-            print("Enter A password")
-            password = input()
-            print("\n")
-            print("Enter your email")
-            email = input()
-            print("\n")
+    #     print("Use the following shortcodes: ca - to create a account, lg - to login")
+    #     shortcodes = input().lower()
+    #
+    #     if shortcodes == 'ca':
+    #
+    #
+    #
+    #     elif shortcodes == 'lg':
+    #         login()
+    #
+    #
+    #         user = User()
 
 
-            handle = open("credentials.txt","a")
-
-            handle.write(username)
-            handle.write(" ")
-            handle.write(password)
-            handle.write("\n")
-            handle.write(email)
-            handle.write("\n")
-
-
-            handle.close()
-
-
-
-        elif shortcodes == 'lg':
-            print("create a login function")
-
-    add_user()
+    all()
