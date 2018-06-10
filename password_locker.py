47b781e2301620ff87f09178cee69400ec92eb99
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-06-10 19:20:14 +0300

    renamed file and added clas user

---
+++ password_locker.py
@@ -0,0 +1,15 @@
+class User:
+    """
+    Class that generates new instances of Users
+    """
+    user_list = []
+
+    def __init__(self,user_name,password,email):
+
+      """
+      init method that defines properties for our objects
+      """
+
+    self.user_name = username
+    self.password = password
+    self.email = email
